36cb1a183343ab371c033ce5ad1e64140f77fb53
KTheXIII <pratchaya.k.git@gmail.com>
2021-04-27 03:09:12 +0200

    Random emotes

--- src/components/Emote.tsx
+++ src/components/Emote.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+
+const emoteList = [
+  '(o _ o)',
+  '(>_<)',
+  '(っ˘̩╭╮˘̩)っ',
+  '(×_×)',
+  '(×﹏×)',
+  '(＋_＋)',
+  '(・・;)ゞ',
+  '(・_・)',
+  '( ͠° ͟ʖ ͡°)',
+  '( ಠ ʖ̯ ಠ)',
+  '(￣ ￣|||)',
+  '(`ー´)',
+  '¯\\_(ツ)_/¯',
+]
+
 export function Emote(): JSX.Element {
+  const [emote, setEmote] = useState('(>_<)')
+
   return (
-    <div className="board">
-      <p>Hello, World!</p>
+    <div className="emote-container">
+      <p
+        id="emote-display"
+        className="noselect"
+        onClick={() => {
+          const i = Math.floor(Math.random() * emoteList.length)
+          setEmote(emoteList[i])
+        }}>{emote}</p>
     </div>
   )
 }
