e13954d93e730c1155d270b613f707e7f64b3440
KTheXIII <pratchaya.k.git@gmail.com>
2021-05-20 13:47:30 +0200

    Animation transition on load delay
    The color transition is now not on page load.

--- src/components/AppTheme.tsx
+++ src/components/AppTheme.tsx
@@ -21,9 +21,18 @@ function userTheme() {
 export const AppTheme = (): ReactElement => {
   const [isThemeHidden, setIsThemeHidden] = useState(true)
   const themeRef = useRef<HTMLDivElement | null>(null)
+  let transitionTimeout: number
+  const TRANSITION_ON_START_DELAY = 500
 
   useEffect(() => {
     document.body.className = userTheme()
+    if (!transitionTimeout) {
+      transitionTimeout = window.setTimeout(() => {
+        const root = document.querySelector('#root')
+        root?.classList.remove('color-transition')
+        root?.classList.add('color-transition')
+      }, TRANSITION_ON_START_DELAY)
+    }
   }, [])
 
   function onClickOutsideTheme(event: MouseEvent) {
